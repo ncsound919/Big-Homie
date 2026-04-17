ce5b8178e5b610d20de47c426dd3bcec2be398a0
Overlay Eco <ncsound919@gmail.com>
2026-04-16 23:55:19 -0400

    feat: add big_homie/revenue package

---
+++ big_homie/revenue/__init__.py
@@ -0,0 +1,15 @@
+"""big_homie.revenue - SaaS generation, content factory, site builder, rap video engine."""
+
+from revenue_engine import RevenueEngine  # noqa: F401
+from saas_spinner import SaasSpinner  # noqa: F401
+from content_factory import ContentFactory  # noqa: F401
+from site_builder import SiteBuilder  # noqa: F401
+from rap_video_engine import RapVideoEngine  # noqa: F401
+
+__all__ = [
+    "RevenueEngine",
+    "SaasSpinner",
+    "ContentFactory",
+    "SiteBuilder",
+    "RapVideoEngine",
+]
